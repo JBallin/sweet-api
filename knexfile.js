2dac3bc54918bff2a85654afb0c378b6c37c53a0
JBallin <JBallin@users.noreply.github.com>
2018-08-09 10:59:45 -0700

    Knex init

---
+++ knexfile.js
@@ -0,0 +1,17 @@
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/sweet_dev'
+  },
+
+  testing: {
+    client: 'pg',
+    connection: 'postgres://localhost/sweet_test',
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
+  }
+  
+};
