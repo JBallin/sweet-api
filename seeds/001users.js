4725720ba7f73ed45601fc1a22ae0a298898f756
JBallin <JBallin@users.noreply.github.com>
2018-08-15 11:58:41 -0700

    Pull seed gist_id from .env

--- seeds/001users.js
+++ seeds/001users.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 exports.seed = knex => (
   knex('users').del()
     .then(() => knex('users').insert([
@@ -6,6 +8,7 @@ exports.seed = knex => (
         name: 'J Ballin',
         email: 'jballin@fake.com',
         hashed_pwd: '$2b$10$UA1gR2UoCttavzzHcEj6X.HVr4HnAAHF8TQltdbeQCd/8tUOHXoIS',
+        gist_id: process.env.JBALLIN_GIST_ID,
       },
     ]))
 );
