8a4eca02cf1d672e549503132b39e7dd1fb4890a
JBallin <JBallin@users.noreply.github.com>
2018-08-18 13:00:25 -0700

    Create API landing page listing available routes

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 /* GET home page. */
 router.get('/', (req, res) => {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'My Sweet Config API' });
 });
 
 module.exports = router;
