01730f86db0935a76d51281883144466714fcd37
JBallin <JBallin@users.noreply.github.com>
2018-08-18 12:09:05 -0700

    Make username unique in migration

--- migrations/20180809111859_users.js
+++ migrations/20180809111859_users.js
@@ -1,7 +1,7 @@
 exports.up = knex => (
   knex.schema.createTable('users', (table) => {
     table.increments();
-    table.string('username', 32).notNullable();
+    table.string('username', 32).unique().notNullable();
     table.string('name', 64).notNullable();
     table.string('email').unique().notNullable();
     table.string('hashed_pwd').notNullable();
