40e83811306889212b9b3afbc5c7c11555e23550
JBallin <JBallin@users.noreply.github.com>
2018-08-20 18:24:20 -0700

    Init /files

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const categoriesRouter = require('./routes/categories');
 const fileTypesRouter = require('./routes/fileTypes');
+const filesRouter = require('./routes/files');
 
 const app = express();
 
@@ -31,6 +32,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/categories', categoriesRouter);
 app.use('/fileTypes', fileTypesRouter);
+app.use('/files', filesRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
