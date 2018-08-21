21958d4b5e7433fff048204c7ddf6ebc3370274a
JBallin <JBallin@users.noreply.github.com>
2018-08-20 22:06:15 -0700

    Test /files

---
+++ test/files.test.js
@@ -0,0 +1,16 @@
+const request = require('supertest');
+const app = require('../app');
+const knex = require('../knex');
+
+describe('files', () => {
+  beforeEach(() => knex.migrate.rollback()
+    .then(() => knex.migrate.latest())
+    .then(() => knex.seed.run()));
+
+  it('should return JSON', (done) => {
+    request(app)
+      .get('/files')
+      .expect('Content-Type', /json/)
+      .expect(200, done);
+  });
+});
