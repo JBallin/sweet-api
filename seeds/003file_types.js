35b2be9948297cae9a6af41fb67b9395ec0a5f8f
JBallin <JBallin@users.noreply.github.com>
2018-08-20 21:18:59 -0700

    Change 'categories_id' to 'category_id'

--- seeds/003file_types.js
+++ seeds/003file_types.js
@@ -4,123 +4,123 @@ exports.seed = knex => (
       {
         title: 'bash_profile',
         extension: '.sh',
-        categories_id: 1,
+        category_id: 1,
       },
       {
         title: 'zshrc',
         extension: '.sh',
-        categories_id: 1,
+        category_id: 1,
       },
       {
         title: 'profile',
         extension: '.sh',
-        categories_id: 1,
+        category_id: 1,
       },
       {
         title: 'bashrc',
         extension: '.sh',
-        categories_id: 1,
+        category_id: 1,
       },
       {
         title: 'bash_completions',
-        categories_id: 1,
+        category_id: 1,
       },
       {
         title: 'brew_list',
-        categories_id: 2,
+        category_id: 2,
       },
       {
         title: 'brew_leaves',
-        categories_id: 2,
+        category_id: 2,
       },
       {
         title: 'brew_cask',
-        categories_id: 2,
+        category_id: 2,
       },
       {
         title: 'brew_services',
-        categories_id: 2,
+        category_id: 2,
       },
       {
         title: 'gitignore_global',
-        categories_id: 3,
+        category_id: 3,
       },
       {
         title: 'gitconfig',
-        categories_id: 3,
+        category_id: 3,
       },
       {
         title: 'npm_global',
-        categories_id: 4,
+        category_id: 4,
       },
       {
         title: 'nvmrc',
-        categories_id: 4,
+        category_id: 4,
       },
       {
         title: 'apm',
-        categories_id: 5,
+        category_id: 5,
       },
       {
         title: 'atom_config',
         extension: '.cson',
-        categories_id: 5,
+        category_id: 5,
       },
       {
         title: 'atom_keymap',
         extension: '.cson',
-        categories_id: 5,
+        category_id: 5,
       },
       {
         title: 'atom_snippets',
         extension: '.cson',
-        categories_id: 5,
+        category_id: 5,
       },
       {
         title: 'atom_styles',
         extension: '.less',
-        categories_id: 5,
+        category_id: 5,
       },
       {
         title: 'atom_init',
         extension: '.coffee',
-        categories_id: 5,
+        category_id: 5,
       },
       {
         title: 'vs_settings',
         extension: '.json',
-        categories_id: 6,
+        category_id: 6,
       },
       {
         title: 'vs_extensions',
-        categories_id: 6,
+        category_id: 6,
       },
       {
         title: 'brackets_settings',
         extension: '.json',
-        categories_id: 6,
+        category_id: 6,
       },
       {
         title: 'brackets_keymap',
         extension: '.json',
-        categories_id: 6,
+        category_id: 6,
       },
       {
         title: 'brackets_extensions',
-        categories_id: 6,
+        category_id: 6,
       },
       {
         title: 'brackets_disabled_extensions',
         extension: '.json',
-        categories_id: 6,
+        category_id: 6,
       },
       {
         title: 'vimrc',
-        categories_id: 7,
+        category_id: 7,
       },
       {
         title: 'nanorc',
-        categories_id: 7,
+        category_id: 7,
       },
     ]))
 );
