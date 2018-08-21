1766dac41c2972449db8134039462d5d9add69b4
JBallin <JBallin@users.noreply.github.com>
2018-08-20 20:22:16 -0700

    Write fetchGistFiles returning array of files in gist

---
+++ api/gist.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const fetch = require('node-fetch');
+
+const gistAPI = 'https://api.github.com/gists';
+
+async function fetchGistFiles(gistID) {
+  let files = [];
+  if (gistID) {
+    const gistFetch = await fetch(`${gistAPI}/${gistID}`);
+    const gist = await gistFetch.json();
+    files = Object.keys(gist.files);
+  }
+  return files;
+}
+
+module.exports = fetchGistFiles;
