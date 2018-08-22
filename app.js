ec89617b941f46edfa6790b8292424dab898d299
JBallin <JBallin@users.noreply.github.com>
2018-08-21 19:50:57 -0700

    Use localhost:3000 for CORS if process.env.APP_URL is undefined

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', process.env.APP_URL);
+  res.header('Access-Control-Allow-Origin', process.env.APP_URL || 'http://localhost:3000');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
