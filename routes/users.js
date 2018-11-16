3f8a7bc36052954dc622da5615ac48ef9ece6763
JBallin <JBallin@users.noreply.github.com>
2018-11-15 16:29:09 -0800

    Only respond with id, username, and name in /users

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,8 @@ const router = require('express').Router();
 const knex = require('../knex');
 const { createError } = require('../utils/errors');
 
-router.get('/', (req, res) => {
-  knex('users')
+router.get('/', (req, res, next) => {
+  knex('users').select('id', 'username', 'name')
     .then(users => res.json(users))
     .catch(err => next(createError(500, 'Error fetching users table', err)));
 });
