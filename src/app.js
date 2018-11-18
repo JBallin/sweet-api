7c4fe7607d24e4cdbe02d25a1224030451a7d952
JBallin <JBallin@users.noreply.github.com>
2018-11-17 17:29:35 -0800

    Only use logger (morgan) in development

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ app.disable('x-powered-by');
 app.set('views', path.join(__dirname, '..', 'views'));
 app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
+if (!process.env.NODE_ENV) app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
