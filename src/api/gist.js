cae4d9a91ea5c03c96ba32543aaf45df8d405790
JBallin <JBallin@users.noreply.github.com>
2018-11-17 16:19:53 -0800

    Use fetchGist helper function

--- src/api/gist.js
+++ src/api/gist.js
@@ -4,11 +4,15 @@ const gistAPI = 'https://api.github.com/gists';
 const { GITHUB_TOKEN } = process.env;
 const token = GITHUB_TOKEN ? `access_token=${GITHUB_TOKEN}` : '';
 
-async function fetchGistFiles(gistID) {
+async function fetchGist(gistId) {
+  const gistJSON = await fetch(`${gistAPI}/${gistId}?${token}`);
+  return gistJSON.json();
+}
+
+async function fetchGistFiles(gistId) {
   let files = [];
-  if (gistID) {
-    const gistFetch = await fetch(`${gistAPI}/${gistID}?${token}`);
-    const gist = await gistFetch.json();
+  if (gistId) {
+    const gist = await fetchGist(gistId);
     const { message } = gist;
     if (message) {
       if (message.includes('rate limit')) throw Error('GitHub API rate limit exceeded');
