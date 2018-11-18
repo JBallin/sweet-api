a2d9bd79a83c0a864a14e8bf4c4f144270bc0651
JBallin <JBallin@users.noreply.github.com>
2018-11-17 20:46:07 -0800

    Test POST /users

--- test/users.test.js
+++ test/users.test.js
@@ -36,6 +36,48 @@ describe('/users', () => {
         });
     });
   });
+  describe('POST', () => {
+    it('should create user', (done) => {
+      request(app)
+        .post('/users')
+        .send(seedUser1)
+        .expect(201)
+        .expect('Content-Type', /json/)
+        .end((err, res) => {
+          if (err) return done(err);
+          const { username, name } = seedUser1;
+          assert.deepEqual(res.body, {
+            username, name, id: seeds.length,
+          });
+          return done();
+        });
+    });
+    it('should error with empty body', (done) => {
+      request(app)
+        .post('/users')
+        .expect(400)
+        .expect('Content-Type', /html/)
+        .end((err, res) => {
+          if (err) return done(err);
+          assert.equal(res.text, 'ERROR: Invalid body');
+          return done();
+        });
+    });
+    it('should error with invalid body', (done) => {
+      request(app)
+        .post('/users')
+        .send(badUser)
+        .expect(400)
+        .expect('Content-Type', /html/)
+        .end((err, res) => {
+          if (err) return done(err);
+          assert.equal(res.text, 'ERROR: Invalid body');
+          return done();
+        });
+    });
+  });
+});
+
         .expect('Content-Type', /json/)
         .expect(200, done);
     });
