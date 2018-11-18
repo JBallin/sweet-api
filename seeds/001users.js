2e10ffde80e56890a07925c0f26ff646d123fad7
JBallin <JBallin@users.noreply.github.com>
2018-11-17 20:45:51 -0800

    Test GET /users

--- seeds/001users.js
+++ seeds/001users.js
@@ -1,14 +1,16 @@
 require('dotenv').config();
 
-exports.seed = knex => (
+const seeds = [{
+  username: 'JBallin',
+  name: 'J Ballin',
+  email: 'jballin@fake.com',
+  hashed_pwd: '$2b$10$UA1gR2UoCttavzzHcEj6X.HVr4HnAAHF8TQltdbeQCd/8tUOHXoIS',
+  gist_id: process.env.GIST_ID,
+}];
+
+const seed = knex => (
   knex('users').del()
-    .then(() => knex('users').insert([
-      {
-        username: 'JBallin',
-        name: 'J Ballin',
-        email: 'jballin@fake.com',
-        hashed_pwd: '$2b$10$UA1gR2UoCttavzzHcEj6X.HVr4HnAAHF8TQltdbeQCd/8tUOHXoIS',
-        gist_id: process.env.GIST_ID,
-      },
-    ]))
+    .then(() => knex('users').insert(seeds))
 );
+
+module.exports = { seeds, seed };
--- test/users.test.js
+++ test/users.test.js
@@ -1,11 +1,41 @@
 const request = require('supertest');
+const { assert } = require('chai');
 const app = require('../src/app');
+const { seeds } = require('../seeds/001users');
+const knex = require('../knex');
+
+const seedUser1 = {
+  username: 'super_coder',
+  name: 'Linus Torvalds',
+  email: 'git_creator@gmai.com',
+  id: 2,
+  gistId: 1234,
+  password: 'hello',
+};
+
+const badUser = {
+  nombre: 'paul',
+  content: 'this thing on?',
+  followup: 'anyone there?',
+};
 
 describe('/users', () => {
   describe('GET', () => {
-    it('should return JSON', (done) => {
+    it('should return users', (done) => {
       request(app)
         .get('/users')
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .end((err, res) => {
+          if (err) return done(err);
+          const { username, name } = seeds[0];
+          assert.deepEqual(res.body[0], {
+            username, name, id: 1,
+          });
+          return done();
+        });
+    });
+  });
         .expect('Content-Type', /json/)
         .expect(200, done);
     });
