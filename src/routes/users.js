a7a414f1cdddbb219d24066ffdbe98ee39c6782a
JBallin <JBallin@users.noreply.github.com>
2018-11-20 00:40:22 -0800

    Add validateUserUpdate middleware to PUT /users/:id

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const ctrl = require('../controllers/users');
-const { validateUser, validateId } = require('../utils/validators');
+const { validateUser, validateId, validateUserUpdate } = require('../utils/validators');
 
 const router = express.Router();
 
 router.get('/', ctrl.getAllUsers);
 router.get('/:id', validateId, ctrl.getUser);
 router.post('/', validateUser, ctrl.createUser);
-router.put('/:id', validateId, validateUser, ctrl.editUser);
+router.put('/:id', validateId, validateUserUpdate, ctrl.updateUser);
 router.delete('/:id', validateId, ctrl.deleteUser);
 
 module.exports = router;
