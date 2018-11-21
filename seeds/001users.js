9d80fed458e340b65b5ee36810da50225d361fd4
JBallin <JBallin@users.noreply.github.com>
2018-11-20 19:40:47 -0800

    Update hashed password in seed

--- seeds/001users.js
+++ seeds/001users.js
@@ -5,7 +5,7 @@ const seeds = [{
   name: 'J Ballin',
   email: 'jballin@fake.com',
   username: 'JBallin',
-  hashed_pwd: '$2b$10$UA1gR2UoCttavzzHcEj6X.HVr4HnAAHF8TQltdbeQCd/8tUOHXoIS',
+  hashed_pwd: '$2b$10$fn09RI9qU6OHe6/89oNMn.q/VshQc8V3kzg6x81XvtNJYLUnSd79W',
 }];
 
 const seed = knex => (
