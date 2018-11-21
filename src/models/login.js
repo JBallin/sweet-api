f2aa212dcc3da1579cd5795846ffda419e3cbc75
JBallin <JBallin@users.noreply.github.com>
2018-11-20 19:41:35 -0800

    Add login model

---
+++ src/models/login.js
@@ -0,0 +1,24 @@
+const bcrypt = require('bcrypt');
+const knex = require('../../knex');
+const { createError } = require('../utils/errors');
+
+const login = async ({ email, password }) => {
+  const loginErr = createError(400, 'Invalid credentials. Please try again.');
+  let user;
+  let isPasswordValid;
+  try {
+    user = await knex('users').where('email', email).first();
+    if (!user) return loginErr;
+  } catch (e) {
+    return createError(500, 'Error fetching users', e);
+  }
+  try {
+    isPasswordValid = bcrypt.compareSync(password, user.hashed_pwd);
+    if (!isPasswordValid) return loginErr;
+    return { validCredentials: true };
+  } catch (e) {
+    return createError(500, 'Error authenticating password', e);
+  }
+};
+
+module.exports = { login };
