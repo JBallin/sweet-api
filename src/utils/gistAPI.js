9ea866ceea799731fc928cb755f188a98f258158
JBallin <JBallin@users.noreply.github.com>
2018-11-21 15:57:34 -0800

    Add error handling to fetchGist()

--- src/utils/gistAPI.js
+++ src/utils/gistAPI.js
@@ -1,28 +1,32 @@
 const fetch = require('node-fetch');
+const { createError } = require('./errors');
 
 const gistAPI = 'https://api.github.com/gists';
 const { GITHUB_TOKEN } = process.env;
 const token = GITHUB_TOKEN ? `access_token=${GITHUB_TOKEN}` : '';
 
 async function fetchGist(gistId) {
-  const gistJSON = await fetch(`${gistAPI}/${gistId}?${token}`);
-  return gistJSON.json();
-}
-
-async function isValidGist(gistId) {
-  const gist = await fetchGist(gistId);
-  return !!gist.files && !!gist.files['.MyConfig.md'];
+  const gistAPIwithIdAndToken = `${gistAPI}/${gistId}?${token}`;
+  const gistJSON = await fetch(gistAPIwithIdAndToken);
+  const gist = await gistJSON.json();
+  const { message } = gist;
+  if (message) {
+    if (message.includes('rate limit')) {
+      return createError(500, 'GitHub API rate limit exceeded');
+    }
+    if (message === 'Not Found') {
+      return createError(400, `No gist with id '${gistId}'`);
+    }
+    return createError(500, 'Error fetching gist', message);
+  }
+  return gist;
 }
 
 async function fetchGistFiles(gistId) {
   let files = [];
   if (gistId) {
     const gist = await fetchGist(gistId);
-    const { message } = gist;
-    if (message) {
-      if (message.includes('rate limit')) throw Error('GitHub API rate limit exceeded');
-      else throw Error(message);
-    }
+    if (gist.error) return gist;
     files = Object.keys(gist.files);
   }
   return files;
