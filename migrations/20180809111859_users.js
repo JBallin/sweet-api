6d414bcaa74bef571a5fcaf7c32163b96f868010
JBallin <JBallin@users.noreply.github.com>
2018-11-25 12:01:45 -0800

    [migrations] Make limits constants and export them

--- migrations/20180809111859_users.js
+++ migrations/20180809111859_users.js
@@ -1,13 +1,21 @@
-exports.up = knex => (
+const gistIdLimit = 36;
+const usernameLimit = 36;
+const nameLimit = 64;
+
+const up = knex => (
   knex.schema.createTable('users', (table) => {
-    table.string('gist_id', 32).unique().notNullable();
-    table.string('name', 64).notNullable();
     table.uuid('id').notNullable().primary();
+    table.string('gist_id', gistIdLimit).unique().notNullable();
+    table.string('name', nameLimit).notNullable();
     table.string('email').unique().notNullable();
-    table.string('username', 32).unique().notNullable();
+    table.string('username', usernameLimit).unique().notNullable();
     table.string('hashed_pwd').notNullable();
     table.timestamps(true, true);
   })
 );
 
-exports.down = knex => knex.schema.dropTable('users');
+const down = knex => knex.schema.dropTable('users');
+
+module.exports = {
+  nameLimit, usernameLimit, up, down,
+};
