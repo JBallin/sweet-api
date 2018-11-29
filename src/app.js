742ba943db40ff00635e14d6e96d53befa48b5f1
JBallin <JBallin@users.noreply.github.com>
2018-11-28 18:30:15 -0800

    Simplify cors set up

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 require('dotenv').config();
 
 const usersRouter = require('./routes/users');
@@ -21,11 +22,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '..', 'public')));
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', process.env.APP_URL || 'http://localhost:3000');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
+const corsOptions = {
+  origin: process.env.APP_URL || 'http://localhost:3000',
+  credentials: true,
+};
+app.use(cors(corsOptions));
 
 app.use('/users', usersRouter);
 app.use('/categories', categoriesRouter);
