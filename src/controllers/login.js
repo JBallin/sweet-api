62382ff622912857c73864220127a02e89845df7
JBallin <JBallin@users.noreply.github.com>
2018-11-28 22:19:01 -0800

    Send cookie with token on POST login

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -1,9 +1,13 @@
 const model = require('../models/login');
+const { sendTokenCookie } = require('../utils/auth');
 
 const login = async (req, res, next) => {
   const user = await model.login(req.body);
   if (user.error) next(user.error);
-  else res.json(user);
+  else {
+    sendTokenCookie(user, res);
+    res.json({ username: user.username });
+  }
 };
 
 module.exports = { login };
