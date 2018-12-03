e7d3d4041cc95bb099fd4391c198d2337d0f14a2
JBallin <JBallin@users.noreply.github.com>
2018-12-02 22:09:24 -0800

    [users/routes] add validateJWT to auth'd routes

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const ctrl = require('../controllers/users');
 const {
-  validateUser, validateId, validateUserUpdate, validateGistId,
+  validateUser, validateId, validateUserUpdate, validateGistId, validateJWT,
 } = require('../utils/validators');
 
 const router = express.Router();
 
 router.get('/', ctrl.getAllUsers);
-router.get('/:id', validateId, ctrl.getUser);
+router.get('/:id', validateId, validateJWT, ctrl.getUser);
 router.post('/', validateUser, validateGistId, ctrl.createUser);
-router.put('/:id', validateId, validateUserUpdate, ctrl.updateUser);
-router.delete('/:id', validateId, ctrl.deleteUser);
+router.put('/:id', validateId, validateUserUpdate, validateJWT, ctrl.updateUser);
+router.delete('/:id', validateId, validateJWT, ctrl.deleteUser);
 
 module.exports = router;
