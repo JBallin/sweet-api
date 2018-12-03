07e79597d1e84b423e1d4fa3523bc70d22decdd1
JBallin <JBallin@users.noreply.github.com>
2018-12-02 17:15:57 -0800

    [seed] change username/email

--- seeds/001users.js
+++ seeds/001users.js
@@ -4,8 +4,8 @@ const seeds = [{
   id: '1ee370d1-2ef3-4c0e-b0f3-6ffccc697dd0',
   gist_id: process.env.GIST_ID || '5fb484f659ccc54a493d4295d6346a39',
   name: null,
-  email: 'jballin@fake.com',
-  username: 'jballin',
+  username: 'seed_user',
+  email: 'seed@fake.com',
   hashed_pwd: '$2b$10$fn09RI9qU6OHe6/89oNMn.q/VshQc8V3kzg6x81XvtNJYLUnSd79W',
 }];
 
