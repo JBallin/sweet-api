b3a3a461425b261705bcd653abaa669eb7546975
JBallin <JBallin@users.noreply.github.com>
2018-12-03 11:24:02 -0800

    Turn seed_user into demo_user

--- seeds/001users.js
+++ seeds/001users.js
@@ -2,8 +2,8 @@ const seeds = [{
   id: '1ee370d1-2ef3-4c0e-b0f3-6ffccc697dd0',
   gist_id: '5fb484f659ccc54a493d4295d6346a39',
   name: null,
-  username: 'seed_user',
-  email: 'seed@fake.com',
+  username: 'demo_user',
+  email: 'demo@gmail.com',
   hashed_pwd: '$2b$10$fn09RI9qU6OHe6/89oNMn.q/VshQc8V3kzg6x81XvtNJYLUnSd79W',
 }];
 
