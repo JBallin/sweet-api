6ce289d824a23003b884fd3a5df03fc9ff2ce2f6
JBallin <JBallin@users.noreply.github.com>
2018-12-08 18:22:17 -0800

    [controllers/login] send entire req to models (for req.tokenId access)

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -2,7 +2,7 @@ const model = require('../models/login');
 const { sendTokenCookie } = require('../utils/auth');
 
 const login = async (req, res, next) => {
-  const user = await model.login(req.body);
+  const user = await model.login(req);
   if (user.error) next(user.error);
   else {
     sendTokenCookie(user, res);
