cfdfc6ef058d8602802f46ac562a6aa8f3c01630
JBallin <JBallin@users.noreply.github.com>
2019-01-21 16:05:30 -0800

    Fix GET /fileTypes to fetch proper db table

--- src/models/fileTypes.js
+++ src/models/fileTypes.js
@@ -1,6 +1,6 @@
 const knex = require('../../knex');
 const errors = require('../utils/errors');
 
-const getFileTypes = () => knex('categories').catch(e => errors.fetchDB('file_types', e));
+const getFileTypes = () => knex('file_types').catch(e => errors.fetchDB('file_types', e));
 
 module.exports = { getFileTypes };
